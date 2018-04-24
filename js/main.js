d17f2f7ad6f1b89bc445cec04a8f9a8a0d970375
Juan Salvatore <juansalvatore@live.com.ar>
2018-04-24 12:42:03 -0300

    Activa las animaciones al hacer scroll y ajusta estilo navbar

--- js/main.js
+++ js/main.js
@@ -12,9 +12,9 @@ $(document).ready(function() {
       triggerHook: 0.8,
     })
       .setClassToggle(this, 'fade-in') // add class 'fade-in' to elements with class 'listen'
-        .addIndicators({
-          name: 'fade in',
-        })
+        // .addIndicators({
+        //   name: 'fade in',
+        // })
       .addTo(controller)
   })
 
@@ -41,4 +41,65 @@ $(document).ready(function() {
       // })
       .addTo(controller)
   }
+
+  // loop through each .listen element
+  // PRIMERA ANIMACION
+  $('.listen-animation').each(function() {
+    // buld a scene
+    var ourScene = new ScrollMagic.Scene({
+      triggerElement: this,
+      //duration: '90%',
+        reverse: false,
+      triggerHook: 0.8,
+    })
+
+      .setClassToggle(this, 'start-animation') // add class 'fade-in' to elements with class 'listen'
+        .addIndicators({
+          name: 'fade in',
+        })
+      .on('start', function () {
+          var svgContainer = document.getElementById('quickandeasy');
+          var autoplay = true
+          var animItem = lottie.loadAnimation({
+              wrapper: svgContainer,
+              animType: 'svg',
+              loop: false,
+              autoplay: autoplay,
+              path: 'https://gist.githubusercontent.com/capitantoto/f60541813fe76b09ad9f61a2d111bb24/raw/99754d0d52df5de1f0b5ccfe455530f64153179a/quickandeasy.json'
+          });
+      })
+      .addTo(controller)
+  })
+  // SEGUNDA ANIMACION
+  $('.listen-animation-two').each(function() {
+    // buld a scene
+    var ourScene = new ScrollMagic.Scene({
+      triggerElement: this,
+      //duration: '90%',
+        reverse: false,
+      triggerHook: 0.8,
+    })
+
+      .setClassToggle(this, 'start-animation') // add class 'fade-in' to elements with class 'listen'
+        .addIndicators({
+          name: 'fade in',
+        })
+      .on('start', function () {
+        var svgContainer = document.getElementById('prototype'); var animItem = lottie.loadAnimation({
+            wrapper: svgContainer,
+            animType: 'svg',
+            loop: false,
+            autoplay: true,
+            path: 'https://gist.githubusercontent.com/sebasbarraud/d77d0ffbf6a5e476863330aa190d38a9/raw/a012d956f42689612211f7a7e983bd8c027d5cd5/prototype.json'
+        });
+        setTimeout(() => {
+          lottie.pause()
+        }, 3900)
+      })
+      .addTo(controller)
+  })
+
+
+
+
 }) // document ready ends
